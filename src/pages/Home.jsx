e37881cc5fcc7b40b5e5cc4ac688304be9651fe6
ankan-coder <76089611+ankan-coder@users.noreply.github.com>
2025-07-24 09:44:12 +0530

    Commented out the Patient App Download Modal Button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -526,7 +526,7 @@ const Home = () => {
                     >
                       Get the Doctor App Free
                     </button>
-                    <button
+                    {/* <button
                       className="home-cta-button"
                       onClick={handlePatientModalOpen}
                       onMouseOver={(e) => {
@@ -541,7 +541,7 @@ const Home = () => {
                       }}
                     >
                       Get the Patient App Free
-                    </button>
+                    </button> */}
                   </div>
                 </div>
               </div>
